8ef62bda0ea4460d400a2e85637e59f73fe16f05
Mark Shustorovych <markshustorovych@gmail.com>
2021-02-17 21:02:01 +0300

    dialogs pages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import s from './dialogs.module.css'
 
 const  Dialogs= (props) => {
     return(
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+             <div className={s.dialogsItems}>
+                  <div className={s.item + '' + s.active}>
+                      Nergal
+                     </div> 
+                     <div className={s.item}>
+                       Varg
+                    </div> 
+                     <div className={s.item}>
+                      Faust
+                     </div> 
+                     <div className={s.item}>
+                     Dead
+                    </div> 
+                    </div>
+                 <div className={s.messages}>
+                    <div className={s.message }> hello varg</div>
+                    <div className={s.message}> hello faust</div>
+                    <div className={s.message}> hello alive man</div>
+                 </div>
+             
         </div>
     )
 
